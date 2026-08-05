55b187666451923c9cad06946b067434c6a89a7c
RajInGit1 <156179397+RajInGit1@users.noreply.github.com>
2026-08-05 17:18:28 +0530

    Add files via upload

---
+++ FirstProgram.py
@@ -0,0 +1,22 @@
+'''name = "john"
+a =10
+b=10.5
+print(type(a))
+print(type(b))
+
+c= a+b
+print(c)
+print(type(c))
+
+print(bin(15))
+print(hex(15))
+print(oct(15))
+'''
+'''marks=int(input("enter your marks:"))
+
+if marks>=90:
+    print("you are eligible for discount")
+elif marks>=80 and marks<=90:
+    print("you are eligible for rent")
+else:
+    print("you are not eligible for rent")'''

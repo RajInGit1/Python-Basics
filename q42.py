2a44c754f76f5b55e96c6d993aab3ce36d331447
RajInGit1 <156179397+RajInGit1@users.noreply.github.com>
2026-08-17 21:50:11 +0530

    Add files via upload

---
+++ q42.py
@@ -0,0 +1,8 @@
+# to check list has middle value or not
+
+data= [10,20,30,40,50]
+
+if len(data) % 2 != 0:
+    print("it has middle value")
+else:
+    print("list does not have single middle value")

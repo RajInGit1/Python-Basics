a9c9a1c091e5f94e42e2983444424225091086bb
RajInGit1 <156179397+RajInGit1@users.noreply.github.com>
2026-08-25 12:58:46 +0530

    Add files via upload

---
+++ q47.py
@@ -0,0 +1,57 @@
+# s = 'power star'
+# lst = s.split()
+
+# d = {}
+
+# for i in lst :
+#     d[i] = len(i)
+# print(d)
+
+# s = 'alway keep smilling'
+# lst = s.split()
+# out = []
+
+# for i in lst:
+#     out.append(i[::-1])
+# print(' '.join(out))
+
+# s = 'alway keep smiling'
+# lst = s.split()
+
+# for i in range(len(lst)):
+#     lst[i] = lst[i][::-1]
+# print(' '.join(lst))
+
+
+# s = ['jiocinema.com','file.py','web.html','amazon.com','www.org']
+
+# out = []
+
+# for i in s:
+#     exe = i.split('.')[-1]
+#     out.append(exe)
+# print(out)
+ 
+
+
+
+# s = 'hello world'
+
+# d = {}
+
+# for i in range (len(s)):
+#     d[i] = s[i]
+# print('\n',d,'\n')
+
+
+
+# l = ['no',34,3.4,'nahi','nhi',90,'never']
+
+# d={}
+
+# for i in l:
+#     if type(i) == str:
+#         d[i] = i[0] + i[-1]
+# print(d)
+
+

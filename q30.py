061e0413883b79dda341ea7687441a8e621e6a02
RajInGit1 <156179397+RajInGit1@users.noreply.github.com>
2026-09-12 20:41:48 +0530

    Add files via upload

---
+++ q30.py
@@ -0,0 +1,46 @@
+# nums = [-5,0,3,-1,7,-2]
+
+# positve = lambda n: n > 0
+
+# res = list(filter(positve, nums))
+# print(res)
+
+
+# # to get only words longer than 4 characters from a list of words
+
+# words = ["hello", "world", "python", "is", "awesome"]
+
+# variable = lambda word: len(word) > 4
+
+# res = list(filter(variable, words))
+# print(res)
+
+# to get all strings that start with the letter 'a' 
+
+# names = ["apple", "banana", "Charlie", "David", "Ave", "Frank"]
+
+# variable = lambda n : n[0] == 'a' or n[0] == 'A'
+
+# res = list(filter(variable, names))
+# print(res)
+
+
+# to remove empty strings from a list of strings
+
+# strings = ["hello", "", "world", "", "python", "is", "", "awesome"]
+
+# variable = lambda n: n != ""
+
+# res = list(filter(variable, strings))
+# print(res)
+
+# to keep only palindrome words from a list of words
+
+# words = ["level", "hello", "world", "radar", "python", "is", "madam"]
+
+# variable = lambda n: n == n[::-1]
+
+# res = list(filter(variable, words))
+# print(res)
+
+# to select prime numbers from a list of numbers

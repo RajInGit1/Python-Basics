0b3de23f3c262bafffbdafe7b419069098dc7d04
RajInGit1 <156179397+RajInGit1@users.noreply.github.com>
2026-09-15 11:46:45 +0530

    Add files via upload

---
+++ q32.py
@@ -0,0 +1,20 @@
+# to get list of file names ending with .txt from a list of file names
+
+# files = ["file1.txt", "file2.doc", "file3.txt", "file4.pdf", "file5.txt"]
+
+# var = lambda  n : n.split(".")[-1] == "txt"
+
+# res = tuple(filter(var,files))
+# print(res)
+
+
+# to get perfect squares from a list of numbers
+
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# var = lambda n : (n ** 0.5)**2 == n
+# res = list(filter(var,numbers))
+# print(res)
+
+
+

e6e8301d2c8886602f7b5ddcab00e47f82812974
RajInGit1 <156179397+RajInGit1@users.noreply.github.com>
2026-09-20 12:56:46 +0530

    Add files via upload

---
+++ q37.py
@@ -0,0 +1,16 @@
+'''subject = {'python','sql','c'}
+student = {'ram','john','sam'}
+
+set={(i,j)  for  i in student for j in subject}
+print(set)'''
+
+l1 = [1,2,3,4,]
+l2 = [5,6,7,8]
+
+
+for i in range(l1.len(),l2.len()):
+    for j in l1:
+        a = l1 + l2
+        
+        
+print(a)

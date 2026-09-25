582f85f6b1a238f7915ad24b252050aa2067edd7
RajInGit1 <156179397+RajInGit1@users.noreply.github.com>
2026-09-25 20:29:39 +0530

    Add files via upload

---
+++ q38.py
@@ -0,0 +1,19 @@
+class Bank():
+    bname = "SBI"
+    bloc = "vashi"
+    IFSC = "121003"
+
+cust = Bank ()
+'''#print(cust.bname)
+
+cust1 = Bank()
+cust2 = Bank()
+
+Bank.bname = 'ABC'
+
+cust1.bname='RBI'
+
+print(cust1.bname)
+print(cust2.bname)'''
+
+print(cust)
